35433dc30381d55d98127e7a1fee72a0e6116f3d
Suraj Mourya <mouryasuraj6266444@gmail.com>
2026-06-10 23:32:25 +0530

    chore: completed restapi til 1:50

---
+++ 1. Networking/4.RestAPI/notes.js
@@ -0,0 +1,30 @@
+/*
+
+-------Method---------
+
+ALways follow best practices
+:id in path is called params and some key values pair separated with '&' are called query
+1. GET
+--> URL - /todos
+          /todo/:id
+2. POST
+--> URL - /todos
+-->ReqB - Optional
+3. PUT/PATCH
+--> URL - /todos:/id
+-->ReqB - Mandatory
+4. Delete
+--> URL - /todos:/id
+
+
+
+-----------Header-----------
+Host: https://www.1d.example.com
+Origin: https://www.example.com
+Referer: Referer is an HTTP request header that tells a website where the user came from before visiting the current page.
+
+
+
+
+
+*/
--- 1. Networking/4.RestAPI/server.js
+++ 1. Networking/4.RestAPI/server.js
@@ -4,19 +4,26 @@ import express from 'express'
 const app = express();
 const PORT = 5000
 
-app.use(express.json())
+app.use(express.json()) // This is a middleware, which is used to parse the request body. without this we will get request body as undefined. We can use third party library also (body parser)
+
+const data = [
+    {
+        id:1,
+        name:"Suraj Mourya"
+    }
+]
+
 app.get("/",(req,res)=>{
     console.log("Request", req);
     console.log("Response", res);
-    res.send("I'm up")
+    res.json({data})
 })
 
 
 app.post("/new",(req,res)=>{
     const reqBody = req.body
-    console.log(reqBody)
-
-    res.json(reqBody)
+    data.unshift(reqBody)
+    res.json({data})
 })
 
 
